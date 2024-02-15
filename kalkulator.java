e972049ae653047008a48c3dd867e30067875ed1
AbyasaGavinPunto <agavinp@gmail.com>
2024-02-15 14:56:17 +0700

    Projek Kelompok 4

---
+++ kalkulator.java
@@ -0,0 +1,12 @@
+package hai;
+
+/**
+ * kalkulator
+ */
+public class kalkulator {
+    public static void main(String[] args) {
+        String text = "Gavin";
+        System.out.println(text);
+    }
+
+}
